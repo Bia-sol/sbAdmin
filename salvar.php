95113c1ab26d2a7d6874e25d141c8e13969069f0
beatriz <126676526+Bia-sol@users.noreply.github.com>
2024-10-11 08:58:42 -0300

    Initial commit

---
+++ salvar.php
@@ -0,0 +1,25 @@
+<?php 
+    echo "Hello World";
+    echo"NOME<br>";
+    echo $_POST["nome"];
+    echo"<br><br>";
+    echo"CPF<br>";
+    echo $_POST["cpf"];
+    echo"<br><br>";
+    echo"ENDEREÇO<br>";
+    echo $_POST["endereco"];
+    echo"<br><br>";
+    echo"TELEFONE<br>";
+    echo $_POST["telefone"];
+    echo"<br><br>";
+    echo"IDADE<br>";
+    echo $_POST["idade"];
+    echo"<br><br>";
+    echo"E-MAIL<br>";
+    echo $_POST["email"];
+    echo"<br><br>";
+    echo"OBSERVAÇÃO<br>";
+    echo $_POST["observacao"];
+    echo"<br><br>";
+    ?>
+
